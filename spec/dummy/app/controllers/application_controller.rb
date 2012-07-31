7dae57f93392c90b93bfd7b800f33914c00b2d03
Tagaki Aki <a-tagaki@esm.co.jp>
2012-07-31 15:38:34 +0900

    fix dummy app

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+
+  def authenticate!
+    session[:origin] = request.fullpath
+    me! root_url
+  end
 end
